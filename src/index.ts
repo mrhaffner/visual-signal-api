70b8428460d4ce02be360e1d2048cdbe848f2972
mrhaffner <mrhaffner13@gmail.com>
2021-08-21 18:04:53 -0500

    Added subscriptions-transport-ws, graphql-subscriptions, and @graphql-tools\schema libraries. Updated the entry index.ts file to accomadate subscriptions and WebSocket.  Also did some general reformatting to a singleton pattern.  Implemented newBoard subscription which triggers from updating card or list pos.

--- src/index.ts
+++ src/index.ts
@@ -1,175 +1,228 @@
+import { createServer } from 'http';
 import express from 'express';
+import { execute, subscribe } from 'graphql';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { PubSub } from 'graphql-subscriptions';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import mongoose from 'mongoose';
 import List from './list/model';
 import Card from './card/model';
 
-import { ApolloServer, gql } from 'apollo-server-express';
+(async () => {
+  const PORT = 8080;
+  const pubsub = new PubSub();
+  const app = express();
+  const httpServer = createServer(app);
 
-const typeDefs = gql`
-  type List {
-    _id: ID!
-    name: String!
-    pos: Float!
-    cards: [Card]
-  }
+  const typeDefs = gql`
+    type List {
+      _id: ID!
+      name: String!
+      pos: Float!
+      cards: [Card]
+    }
 
-  type Card {
-    _id: ID!
-    name: String!
-    pos: Float!
-    idList: String!
-  }
+    type Card {
+      _id: ID!
+      name: String!
+      pos: Float!
+      idList: String!
+    }
 
-  type Query {
-    allLists: [List]
-    getAllCards: [Card]
-    getCardById(_id: ID!): Card
-  }
+    type Query {
+      allLists: [List]
+      getAllCards: [Card]
+      getCardById(_id: ID!): Card
+    }
 
-  type Mutation {
-    createList(input: CreateList!): List
-    updateListPos(input: UpdateListPosInput!): List
-    deleteList(_id: ID!): ID
-    createCard(input: CreateCard!): Card
-    updateCardPos(input: UpdateCardPosInput!): Card
-    deleteCard(_id: ID!): ID
-  }
+    type Mutation {
+      createList(input: CreateList!): List
+      updateListPos(input: UpdateListPosInput!): List
+      deleteList(_id: ID!): ID
+      createCard(input: CreateCard!): Card
+      updateCardPos(input: UpdateCardPosInput!): Card
+      deleteCard(_id: ID!): ID
+    }
 
-  input CreateList {
-    name: String!
-    pos: Float!
-  }
+    input CreateList {
+      name: String!
+      pos: Float!
+    }
 
-  input UpdateListPosInput {
-    _id: ID!
-    pos: Float!
-  }
+    input UpdateListPosInput {
+      _id: ID!
+      pos: Float!
+    }
 
-  input CreateCard {
-    name: String!
-    pos: Float!
-    idList: String!
-  }
+    input CreateCard {
+      name: String!
+      pos: Float!
+      idList: String!
+    }
 
-  input UpdateCardPosInput {
-    _id: ID!
-    pos: Float!
-    idList: String
-  }
-`;
+    input UpdateCardPosInput {
+      _id: ID!
+      pos: Float!
+      idList: String
+    }
 
-const resolvers = {
-  Query: {
-    allLists: async () => {
-      const lists = await List.aggregate([
-        {
-          $lookup: {
-            from: 'cards',
-            localField: '_id',
-            foreignField: 'idList',
-            pipeline: [
-              {
-                $sort: { pos: 1 },
-              },
-            ],
-            as: 'cards',
+    type Subscription {
+      newBoard: [List]
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      allLists: async () => {
+        const lists = await List.aggregate([
+          {
+            $lookup: {
+              from: 'cards',
+              localField: '_id',
+              foreignField: 'idList',
+              pipeline: [
+                {
+                  $sort: { pos: 1 },
+                },
+              ],
+              as: 'cards',
+            },
           },
-        },
-      ]).sort('pos');
-      return lists;
+        ]).sort('pos');
+        return lists;
+      },
+      //probably don't need this except for testing
+      getAllCards: async () => {
+        return await Card.find();
+      },
+      getCardById: async (_: any, { _id }: any) => {
+        return await Card.findOne({ _id });
+      },
     },
-    //probably don't need this except for testing
-    getAllCards: async () => {
-      return await Card.find();
-    },
-    getCardById: async (_: any, { _id }: any) => {
-      return await Card.findOne({ _id });
-    },
-  },
-  Mutation: {
-    createList: async (_: any, { input }: any) => {
-      const { name, pos } = input;
-      return await List.create({ name, pos });
-    },
-    updateListPos: async (_: any, { input }: any) => {
-      const { _id, pos } = input;
-      return await List.findOneAndUpdate(
-        {
-          _id,
-        },
-        { pos },
-        {
-          new: true,
-        },
-      );
-    },
-    deleteList: async (_: any, { _id }: any) => {
-      try {
-        await List.findOneAndRemove({
+    Mutation: {
+      createList: async (_: any, { input }: any) => {
+        const { name, pos } = input;
+        return await List.create({ name, pos });
+      },
+      updateListPos: async (_: any, { input }: any) => {
+        const { _id, pos } = input;
+        const list = await List.findOneAndUpdate(
+          { _id },
+          { pos },
+          {
+            new: true,
+          },
+        );
+
+        const lists = await List.aggregate([
+          {
+            $lookup: {
+              from: 'cards',
+              localField: '_id',
+              foreignField: 'idList',
+              pipeline: [
+                {
+                  $sort: { pos: 1 },
+                },
+              ],
+              as: 'cards',
+            },
+          },
+        ]).sort('pos');
+        pubsub.publish('BOARD_UPDATED', { newBoard: lists });
+        return list;
+      },
+      deleteList: async (_: any, { _id }: any) => {
+        try {
+          await List.findOneAndRemove({
+            _id,
+          });
+          return _id;
+        } catch (e) {
+          console.log(e);
+          return null;
+        }
+      },
+      createCard: async (_: any, { input }: any) => {
+        const { name, pos, idList } = input;
+        return await Card.create({ name, pos, idList });
+      },
+      //might want a seperate update function for changing card content vs position (with maybe listId depending on if it changes lists) so content/position can be required fields
+      updateCardPos: async (_: any, { input }: any) => {
+        const { _id, pos, idList } = input;
+        const updateObject = { pos };
+        // @ts-ignore comment
+        if (idList) updateObject.idList = idList;
+        const card = await Card.findOneAndUpdate(
+          {
+            _id,
+          },
+          updateObject,
+          {
+            new: true,
+          },
+        );
+
+        const lists = await List.aggregate([
+          {
+            $lookup: {
+              from: 'cards',
+              localField: '_id',
+              foreignField: 'idList',
+              pipeline: [
+                {
+                  $sort: { pos: 1 },
+                },
+              ],
+              as: 'cards',
+            },
+          },
+        ]).sort('pos');
+        pubsub.publish('BOARD_UPDATED', { newBoard: lists });
+
+        return card;
+      },
+      deleteCard: async (_: any, { _id }: any) => {
+        await Card.findOneAndRemove({
           _id,
         });
         return _id;
-      } catch (e) {
-        console.log(e);
-        return null;
-      }
+      },
     },
-    createCard: async (_: any, { input }: any) => {
-      const { name, pos, idList } = input;
-      return await Card.create({ name, pos, idList });
+    Subscription: {
+      newBoard: {
+        subscribe: () => pubsub.asyncIterator(['BOARD_UPDATED']),
+      },
     },
-    //might want a seperate update function for changing card content vs position (with maybe listId depending on if it changes lists) so content/position can be required fields
-    updateCardPos: async (_: any, { input }: any) => {
-      const { _id, pos, idList } = input;
-      const updateObject = { pos };
-      // @ts-ignore comment
-      if (idList) updateObject.idList = idList;
-      return await Card.findOneAndUpdate(
-        {
-          _id,
-        },
-        updateObject,
-        {
-          new: true,
-        },
-      );
-    },
-    deleteCard: async (_: any, { _id }: any) => {
-      await Card.findOneAndRemove({
-        _id,
-      });
-      return _id;
-    },
-  },
-};
+  };
 
-mongoose.connect('mongodb://localhost:27017/test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+  mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  console.log('connected to Mongo');
-});
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('connected to Mongo');
+  });
 
-const startServer = async () => {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  const app = express();
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  const server = new ApolloServer({
+    schema,
+  });
   await server.start();
   server.applyMiddleware({ app });
 
-  // app.get('/', (req, res) => {
-  //   console.log('Apollo GraphQL server is ready');
-  // });
+  SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: server.graphqlPath },
+  );
 
-  const port = 8080;
-
-  app.listen({ port }, () => {
-    console.log(`Server is running at port: ${port}`);
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
   });
-};
-
-startServer();
+})();

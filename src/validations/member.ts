7320a6980ef1bb24af8592d1731be77c64036643
mrhaffner <mrhaffner13@gmail.com>
2021-09-20 14:42:21 -0500

    fix: corrected email validation

--- src/validations/member.ts
+++ src/validations/member.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const NewMemberRules = Joi.object({
   fullName: Joi.string().required(),
   password: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().email().required(),
 });
 
 export default NewMemberRules;
